fb4bb18c483d54ca47c62af8513fc2fa5e0b2a5a
Cristian369 <46296817+Cristian369@users.noreply.github.com>
2023-12-08 23:25:36 +0100

    config.h

---
+++ config.h
@@ -0,0 +1,11 @@
+static const Config configs[] = {
+    {
+        /* wifi_name           */ "mywifiname", //hidden
+        /* wifi_pass           */ "mypass",     //hidden
+        /* type                */ mvg_api,
+        /* bahnhof             */ "de:09162:340", //Petuelring
+	/* include_type        */ {"UBAHN" },
+        /* include_line        */ { "U3" },
+        /* exclude_destination */ { },
+    }
+};
